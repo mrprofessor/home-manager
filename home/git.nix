c2d723171894e3be883990fb81508f9b058d77cf
mrprofessor <rudra.kar@icloud.com>
2025-11-16 04:35:59 +1300

    Update git config to new settings format
    Fix deprecated options in home/git.nix:
    - userName/userEmail → settings.user.name/email
    - extraConfig → settings
    - aliases → settings.alias
    
    Also update flake.lock with latest nixpkgs

--- home/git.nix
+++ home/git.nix
@@ -4,21 +4,23 @@
   programs.git = {
     enable = true;
 
-    userName = "mrprofessor";  # Change this to your name
-    userEmail = "rudra.kar@icloud.com";  # Change this to your email
+    settings = {
+      user = {
+        name = "mrprofessor";
+        email = "rudra.kar@icloud.com";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
-    };
 
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
     };
   };
 }
